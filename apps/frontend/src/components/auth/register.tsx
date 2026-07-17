6b13634673b3eee5f9a2cf356303349632e8ae87
subash <subashrijal5@gmail.com>
2026-07-17 20:36:56 +0900

    ds

--- apps/frontend/src/components/auth/register.tsx
+++ apps/frontend/src/components/auth/register.tsx
@@ -219,7 +219,7 @@ export function RegisterAfter({
                 )}
                 &nbsp;
                 <a
-                  href={`https://postiz.com/terms`}
+                  href={`https://publishr.seiwahld.com/en/terms-of-service`}
                   className="underline hover:font-bold"
                   rel="nofollow"
                 >
@@ -228,7 +228,7 @@ export function RegisterAfter({
                 &nbsp;
                 {t('and', 'and')}&nbsp;
                 <a
-                  href={`https://postiz.com/privacy`}
+                  href={`https://publishr.seiwahld.com/en/privacy-policy`}
                   rel="nofollow"
                   className="underline hover:font-bold"
                 >
